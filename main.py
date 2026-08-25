a43fb6edb0dafd929b517d04bf8dfa46a3666eb5
jloim <joloimer@gmail.com>
2026-08-25 15:02:53 +0200

    before groups

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     #Infinite loop to keep the game running
     while True:
         log_state()
